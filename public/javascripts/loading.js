7c7c3e4ed03d5f432b09c746fa616ecefe6944b7
YASH2204V <yash.v220104@gmail.com>
2024-11-09 16:24:12 +0530

    #4

--- public/javascripts/loading.js
+++ public/javascripts/loading.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', async function() {
-    const res = await fetch('loading.html');
-    const data = await res.text();
-    document.getElementById('loading-screen').innerHTML = data;
-    setTimeout(async function() {
-        document.getElementById('loading-screen').innerHTML = "";
-        window.location.href = 'home.html'; // Redirect to home.html
-    }, 5000);
+    try {
+        const res = await fetch('loading.html');
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await res.text();
+        document.getElementById('loading-screen').innerHTML = data;
+        setTimeout(function() {
+            document.getElementById('loading-screen').innerHTML = "";
+            window.location.href = 'home.html'; // Redirect to home.html
+        }, 5000);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
 });
